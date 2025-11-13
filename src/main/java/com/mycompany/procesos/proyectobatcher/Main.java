7a2c93952e216e9fa5c711dc506fc6dc8d0650ea
Axel Torreiro Lodeiro <a24axeltl@iesantonlosada.gal>
2025-11-13 14:27:28 +0100

    Downgrade del programa para hacerlo compatible con los equipos del aula + Mejora pequeña de la visualización de los estados

--- src/main/java/com/mycompany/procesos/proyectobatcher/Main.java
+++ src/main/java/com/mycompany/procesos/proyectobatcher/Main.java
@@ -9,6 +9,7 @@ import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -45,13 +46,14 @@ public class Main {
         for(Job jobREADY : state.getStateREADY()){
             if(state.getStateRUNNING().isEmpty()){
                 processCreation(jobREADY,processJobs);
-                state.getStateRUNNING().addFirst(jobREADY);
+                state.getStateRUNNING().add(0,jobREADY);
             } else {
                 processCreation(jobREADY,processJobs);
                 state.getStateRUNNING().add(jobREADY);
             }  
         }
         state.getStateREADY().clear();
+        showJobsStates(state);
         
         for(int i = 0; i < processJobs.size(); i++){
             Process processJob = processJobs.get(i);
@@ -66,8 +68,6 @@ public class Main {
                 freeResources(job);
             }
             System.out.println("[HIJO " + processJob.pid() + "]PROCESO TERMINADO CON SALIDA: " + exitCode);
-            
-            i++;
         }
         state.getStateRUNNING().clear();
         showJobsStates(state);
@@ -75,11 +75,25 @@ public class Main {
     
     private static void admisionsJobsToNewState(File jobsDir, State state) throws IOException{
         String[] jobsFiles = jobsDir.list();
-        for(String jobFile : jobsFiles){
-            ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
-            Job jobYAML = objectMapper.readValue(new File("jobs/" + jobFile), Job.class);
-            state.getStateNEW().add(jobYAML);
+        if(jobsFiles != null){
+            Arrays.sort(jobsFiles);
+            for (String jobFile : jobsFiles) {
+                ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
+                Job jobYAML = objectMapper.readValue(new File("jobs/" + jobFile), Job.class);
+                state.getStateNEW().add(jobYAML);
+            }
+        } else {
+            System.out.println("NO se han detectado jobs para procesar. Cerrando el programa");
+            System.exit(0);
         }
+        
+    }
+    
+    private static void freeResources(Job job){
+        cpuCores = cpuCores + job.getResources().getCpu_cores();
+        String[] memoryJob = job.getResources().getMemory().split(" ");
+        int memoryValue = Integer.parseInt(memoryJob[0]);
+        memoryMB = memoryMB + memoryValue;
     }
     
     private static void processCreation(Job jobREADY,List<Process> processJobs) throws IOException{
@@ -95,37 +109,18 @@ public class Main {
         processJobs.add(processJob);
     }
     
-    private static void freeResources(Job job){
-        cpuCores = cpuCores + job.getResources().getCpu_cores();
-        String[] memoryJob = job.getResources().getMemory().split(" ");
-        int memoryValue = Integer.parseInt(memoryJob[0]);
-        memoryMB = memoryMB + memoryValue;
-    }
-    
     private static void showJobsStates(State state){
+        System.out.println("=========================================================================");
         System.out.println("State Jobs:");
-        System.out.println("NEW: " + state.getStateNEW());
-        System.out.println("READY: " + state.getStateREADY());
-        System.out.println("WAITING: " +state.getStateWAITING());
-        System.out.println("---------------------------------------");
-        System.out.println("RUNNING: " +state.getStateRUNNING());
-        System.out.println("DONE: " +state.getStateDONE());
-        System.out.println("FAILED: " +state.getStateFAILED());
+        System.out.println("CPU: " + cpuCores + " | Memory: " + memoryMB);
         System.out.println();
-    }
-    
-    private static ArrayList<Job> priorityOrder(State state){
-        ArrayList<Job> listJob = new ArrayList<>();
-        for(Job jobNEW : state.getStateNEW()){
-            int priority = jobNEW.getPriority();
-            if(priority >= 1){
-                listJob.addFirst(jobNEW);
-            } else if(priority == 4){
-                listJob.addLast(jobNEW);
-            } else {
-                listJob.add(jobNEW);
-            }
-        }
-        return listJob;
-    }
+        System.out.println("NEW("+state.getStateNEW().size()+"): " + state.getStateNEW());
+        System.out.println("READY("+state.getStateREADY().size()+"): " + state.getStateREADY());
+        System.out.println("WAITING("+state.getStateWAITING().size()+"):" + state.getStateWAITING());
+        System.out.println("---------------------------------------");
+        System.out.println("RUNNING("+state.getStateRUNNING().size()+"): " +state.getStateRUNNING());
+        System.out.println("DONE("+state.getStateDONE().size()+"): " +state.getStateDONE());
+        System.out.println("FAILED("+state.getStateFAILED()+": " +state.getStateFAILED());
+        System.out.println("=========================================================================");
+    } 
 }
